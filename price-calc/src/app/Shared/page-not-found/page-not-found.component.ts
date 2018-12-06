3114bc1864f2e362a96eaebe9b6b2357e8fa46b4
Daniel Santos <daniel@brasileiro.ca>
2018-12-05 23:14:07 -0500

    Updated behavior for page not found

--- price-calc/src/app/Shared/page-not-found/page-not-found.component.ts
+++ price-calc/src/app/Shared/page-not-found/page-not-found.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pcalc-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: [ './page-not-found.component.scss' ],
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  secondsToTimeout = 3;
 
-  ngOnInit() {
+  constructor(private router: Router) {
   }
 
+  ngOnInit() {
+    const timer = setInterval(() => {
+        if (this.secondsToTimeout !== 0) {
+          this.secondsToTimeout--;
+        } else {
+          clearInterval(timer);
+          // clearTimeout(timer);
+          this.router.navigate([ '/calculator' ]);
+        }
+      },
+      1000);
+  }
 }
