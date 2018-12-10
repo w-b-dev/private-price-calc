42069ffc4b94c3c0f26eccd5cfe87d7750d5ba5b
Daniel Santos <daniel@brasileiro.ca>
2018-12-09 21:45:44 -0500

    Refactoring through utils module

--- price-calc/src/app/Feature/calc-step2/calc-step2.component.ts
+++ price-calc/src/app/Feature/calc-step2/calc-step2.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CalcStatusService } from 'src/app/Core/calc-status.service';
+import { expandTab } from 'src/app/Shared/utils';
 
 @Component({
   selector: 'pcalc-calc-step2',
@@ -14,24 +15,6 @@ export class CalcStep2Component implements OnInit {
   }
 
   expand(event) {
-    const allElements = event.target.parentElement.parentElement.childNodes;
-    for (const e of allElements) {
-      console.info('💖:', e.localName);
-      if (e.className === 'expanded' && e === event.target.parentElement) {
-        console.info(e.localName, 'ALREADY EXPANDED 😎');
-      }
-      if (e.className === 'expanded' && e !== event.target.parentElement) {
-        e.classList.remove('expanded');
-        e.classList.add('closed');
-      }
-      if (e.className === 'closed' && e === event.target.parentElement) {
-        e.classList.remove('closed');
-        e.classList.add('expanded');
-        this.StatusService.updateTabOpen(e.localName);
-      }
-      if (e.className === 'closed' && e !== event.target.parentElement) {
-        console.info(e.localName, 'ALREADY CLOSED 😴');
-      }
-    }
+    expandTab(event, this.StatusService);
   }
 }
