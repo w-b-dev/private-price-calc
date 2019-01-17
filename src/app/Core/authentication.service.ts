3c10c638753cb44147ed7026446ff29445cf17c8
Daniel Santos <daniel@brasileiro.ca>
2019-01-17 05:56:22 -0500

    Saving authentication state and validation log locally

--- src/app/Core/authentication.service.ts
+++ src/app/Core/authentication.service.ts
@@ -1,44 +1,50 @@
-import { Injectable, OnInit } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as firebase from 'firebase';
 import {environment} from "../../environments/environment";
+import * as storage from "localforage";
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthenticationService implements OnInit {
+export class AuthenticationService {
 
   firebaseObject = environment.firebaseObject;
-
-  firstTime = true;
-  validation = 'Nops...';
+  firstTime: boolean = true;
 
   constructor() {
     firebase.initializeApp(this.firebaseObject);
   }
 
-  ngOnInit() {
+  private changeLoginStatus(){
+    this.firstTime = !this.firstTime;
+  }
+
+  signIn(u, p) {
+    /*TODO: make login*/
+    console.info(`Send this to login API: ${u} and ${p}`)
   }
 
   registerUser(u, p) {
 
-    const authResult = firebase
+    const result = firebase
       .auth()
       .createUserWithEmailAndPassword(u, p)
-      .then(x => {
-        this.firstTime = false;
-        console.info('RETURNED RESPONSE', x);
-        if (x.additionalUserInfo.isNewUser) {
-          this.validation = x.additionalUserInfo.username;
-          return console.info('USER REGISTERED', x.user.email);
-        }
-      })
-      .catch(function (error) {
-        console.error(error.code);
-        console.error(error.message);
-        return error;
-      });
+      .then(x => x)
+      .catch(error => storage.setItem(new Date().toString(), error));
 
-    console.info('AUTH RESULT IS', authResult);
-    console.info('Validation', this.validation);
+    result.then(x => {
+      // console.table(x);
+      if (x.message) {
+        console.error('⛔ Validation message:', x.message)
+      } else {
+        this.changeLoginStatus();
+        console.info('✔ Succeeded: ' + x.operationType + ' for ' + x.user.email);
+        storage.setItem(x.user.email, {
+          logDate: new Date().toString(),
+          metadata: x.user.metadata,
+          isNewUser: x.additionalUserInfo.isNewUser,
+        });
+      }
+    });
   }
 }
